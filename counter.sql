924770589773d414ab88da60ce33ab961c1894bd
Stepan Zibarov <stepanzibarov@gmail.com>
2024-11-13 22:36:58 +0300

    Add counter, modify twosum2

---
+++ counter.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE FUNCTION public.counter (list int[])
+RETURNS TABLE (value int, frequency int)
+LANGUAGE plpgsql AS $$
+    DECLARE
+        element int;
+    BEGIN
+        CREATE TEMPORARY TABLE tmp_table_func_counter (value int, frequency int) ON COMMIT DROP;
+        FOREACH element IN ARRAY list LOOP
+            IF element =
+                    (SELECT tmp_table_func_counter.value
+                    FROM tmp_table_func_counter
+                    WHERE tmp_table_func_counter.value = element) THEN
+
+                UPDATE tmp_table_func_counter
+                SET frequency = tmp_table_func_counter.frequency + 1
+                WHERE tmp_table_func_counter.value = element;
+            ELSE
+                INSERT INTO tmp_table_func_counter (value, frequency)
+                VALUES (element, 1);
+            END IF;
+        END LOOP;
+        RETURN QUERY SELECT * FROM tmp_table_func_counter;
+    END;
+$$
+
+
+
+SELECT * FROM public.counter(ARRAY[1, 7, 0, 3, 2, 1, 3, 7, 7]);
--- twosum2.sql
+++ twosum2.sql
@@ -1,30 +1,48 @@
 ------------------------------------------------------------------------------------------------------------------------
-/*
-nums - массив, target - integer,
-для временного хранения данных используется jsonb
-Возврат в виде массива
-*/
+-- nums - массив, target - integer.
+-- Для временного хранения данных используется jsonb.
+-- Возврат в виде массива.
 ------------------------------------------------------------------------------------------------------------------------
 CREATE OR REPLACE FUNCTION public.twoSum
 (nums int[], target int)
-RETURNS int[] 
+RETURNS RECORD
 LANGUAGE plpgsql AS $$
 
     DECLARE
-        -- TODO добавить переменные
+        curr_value int;
+        difference int;
+        complement int;
+        ind int;
+        answer record;
 
     BEGIN
+
+        ind := 1;
+
+	    -- Declaring temporary table
+	    CREATE TEMPORARY TABLE IF NOT EXISTS temp_hash_map (
+            -- column    -- data type    -- constraints
+            key          int             PRIMARY KEY,
+            value        int);
+
         FOREACH curr_value IN ARRAY nums LOOP
             difference := target - curr_value;
-            IF (hash_map ? difference) THEN
-                second_position := 
-                answer := ARRAY[, curr_position];
-                EXIT;
+            SELECT value INTO complement FROM temp_hash_map WHERE key = difference;
+            IF (complement IS NULL) THEN
+                INSERT INTO temp_hash_map (key, value) VALUES (curr_value, ind);
             ELSE
-                hash_map := hash_map || '{curr_value: curr_position}'::jsonb; 
+                EXIT;
             END IF;
+            ind := ind + 1;
         END LOOP;
+
+        DROP TABLE IF EXISTS temp_hash_map;
+
+        CREATE TEMPORARY TABLE temp_yui (a int, b int);
+        INSERT INTO temp_yui(a, b) VALUES (complement, ind);
+        SELECT * INTO answer FROM temp_yui;
+        DROP TABLE temp_yui;
+        RETURN answer;
+
     END;$$;
 ------------------------------------------------------------------------------------------------------------------------
-
-SELECT {1: 2}:: JSONB;
