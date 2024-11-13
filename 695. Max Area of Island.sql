f28db30ab49a2840dd0aeff9411f9def1da59a6a
Stepan Zibarov <stepanzibarov@gmail.com>
2024-11-14 02:47:22 +0300

    -a .

--- 695. Max Area of Island.sql
+++ 695. Max Area of Island.sql
@@ -1,7 +1,10 @@
 CREATE OR REPLACE FUNCTION public.max_are_of_island
 (grid integer[][]) RETURNS integer LANGUAGE plpgsql AS $$
 DECLARE
+    directions integer[][] := ARRAY[[0, 1], [0, -1], [1, 0], [-1, 0]];
 BEGIN
 END;$$;
 
-select a from VALUES ('{1,2,3,4}') as A;
+CREATE TEMPORARY TABLE asd (lst int[][]);
+INSERT INTO asd VALUES (ARRAY[[1, 2], [3, 4]]);
+SELECT lst[1][1] FROM asd;
